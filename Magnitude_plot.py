2685dcb5438dd0bacac61b3797d6fb772b30d947
Harry Anthony <50590825+HarryAnthony@users.noreply.github.com>
2020-12-31 15:06:25 +0000

    Update Magnitude_plot.py

--- Magnitude_plot.py
+++ Magnitude_plot.py
@@ -32,6 +32,45 @@ Mean_error = np.mean(Data_error)
 Data_error = np.pad(Data_error, 300, mode='constant',constant_values=Mean_error) #Pad the data to stop edge effects
 
 #%%
+#This cell creates a mask array for the data
+
+masked = np.full((Data.shape[0],Data.shape[1]),True,dtype=bool) #Create a 2D boolean array
+
+#Mask the edges of the image
+y = 0
+while y < len(Data)-600:
+    masked[300+y][300:len(Data[0])-300] = np.full(len(Data[0])-600,False)
+    y = y + 1
+
+#Mask the central star
+for y in range(0,39):
+    masked[y+424][860+6*y+242:1460-6*y+242] = np.full(1460-860-12*y,True) 
+for y in range(2470-105,2900+135):
+    masked[y+424][1050-100+242:1500+100+242] = np.full(1500-1050+200,True) 
+for y in range(2670,2900):
+    masked[y+424][1120+242:1290+242] = np.full(1290-1120,True) 
+for y in range(2670,2900):
+    masked[y+424][1120+242:1290+242] = np.full(1290-1120,True) 
+for y in range(0,27):
+    masked[y+424][782+242:803+242] = np.full(803-782,True)
+    
+#Next largest star
+for y in range(2775,3000):
+    masked[y+424][500-19+242:570+19+242] = np.full(570-500+38,True)
+    
+#Next star
+for y in range(2250,2420):
+    masked[y+424][700-19+242:760+19+242] = np.full(760-700+38,True)
+    
+#Next star
+for y in range(1780,1940):
+    masked[y+424][640-19+242:690+19+242] = np.full(690-640+38,True)
+    
+#Next star
+for y in range(3280,3380):
+    masked[y+424][1870-19+242:1920+19+242] = np.full(1920-1870+38,True)
+
+#%%
 #Display the image
 plt.figure(1)
 Show_image(Data)
@@ -39,13 +78,22 @@ plt.show()
 
 #Plot the stars located by the method
 plt.figure(2)
-Show_centres(Data,0.7)
+Show_centres(Data,2,masked)
+
+#%%
+#This cell plots the shapes of the galaxies calculated with and without the watershed algorithm
+
+plt.figure(1)
+Image_no_noise_array(Data,2,masked)
+plt.figure(2)
+Image_no_noise_watershed_array(Data,2,masked)
+
 
 #%%
 #Calculate the counts of each of the galaxies detected
 
-counts_list,centre_list,error_list,background_list = Counts_all_galaxies_circular_aperture(
-        Data,0.7,Data_error)
+counts_list,centre_list,error_list,background_list = Counts_all_galaxies_elliptical_aperture(
+        Data,2,Data_error,mask)
 
 #Convert the counts to magnitude
 MAGZPT  = 2.530E+01
@@ -54,10 +102,12 @@ calibrated_mag = MAGZPT - 2.5* np.log10(counts_list)
 calibrated_mag_err = MAGZRR + 2.5*(np.array(error_list)/np.array(counts_list))
 
 #Write the result into a textfile
-myfile=open('Counts_measurement_circular_survey5.txt','a')
+myfile=open('Counts_measurement_circular_survey.txt','a')
 myfile.write('x'+','+'y'+','+'Magnitude'+','+'Magnitude error'+','+'background'+'\n')
 for x in range(0,len(counts_list)):
-    myfile.write(str(centre_list[x][0]-300+242)+','+str(len(Data)-centre_list[x][1]-300+424)+','+str(calibrated_mag[x])+','+str(calibrated_mag_err[x])+','+str(background_list[x])+'\n')
+    myfile.write(str(centre_list[x][0]-300+242)+','+str(len(Data)-centre_list[x][1]-300+424
+                                                       )+','+str(calibrated_mag[x])+','+str(calibrated_mag_err[x]
+                                                                               )+','+str(background_list[x])+'\n')
 
 myfile.close()
 
@@ -111,8 +161,6 @@ Nm_err = np.sqrt(Nm)
 #Plot the total N(m) against m graph
 plt.figure(1)
 plt.plot(M,np.log10(Nm),'kx')
-#plt.fill_betweenx(np.log10(Nm), np.array(M)-np.array(M_err), np.array(M
-#                  )+np.array(M_err),capstyle='round',lw=0.5, color = (255/256,127/256,80/256), alpha = 1)
 plt.fill_between(M, np.log10(Nm)-Nm_err/Nm, np.log10(Nm)+Nm_err/Nm,
                  capstyle='round',lw=0.5,color = (255/256,127/256,80/256), alpha = 1)
 plt.xlabel('Magnitude M')
@@ -145,10 +193,6 @@ plt.fill_between(M_clipped, np.log10(Nm_clipped)-np.array(Nm_err_clipped
                  )/np.array(Nm_clipped), np.log10(Nm_clipped)+np.array(Nm_err_clipped
                            )/np.array(Nm_clipped), color = (255/256,127/256,80/256
                                      ),capstyle='round',lw=5, alpha=1)
-#plt.fill_betweenx(np.log10(Nm_clipped), np.array(M_clipped)-np.array(M_err_clipped
-#                  ), np.array(M_clipped)+np.array(M_err_clipped), color = (
-#                          255/256,127/256,80/256), capstyle='round',lw=0.5,alpha = 1)
-
 
 #Calculate the linear line of best fit for the data
 fit,cov = np.polyfit(M_clipped, np.log10(Nm_clipped),1, w=np.array(Nm_clipped
@@ -165,7 +209,57 @@ plt.text(13,2.6,'Gradient = '+gradient + ' +/- '+ gradient_err)
 plt.xlabel('Magnitude M')
 plt.ylabel('log$_{10}$(N(M))')
 plt.grid()
-plt.xlim(12,14)
-plt.ylim(2.25,3.25)
+plt.xlim(11,14)
 plt.legend()
 plt.show()
+
+#%%
+#This cell places the galaxies into 0.5 magnitude bins
+
+lognm = np.log10(Nm)
+#Combine the magnitude and logarithm
+combo = [[M[t],lognm[t]] for t in range(0,len(M))]
+x = 7.5 #Start at magnitude 7.5
+bins = []
+#Appends the average in the bin into the array bins
+while x < 22:
+    av = [] 
+    for y in range(0,len(lognm)):
+        if x - 0.5 <= combo[y][0] and x >= combo[y][0]:
+            av.append(combo[y][1])
+    bins.append(np.mean(av))
+    x = x + 0.5
+
+#%%
+#This cells plots the ln(N(m)) vs m plot with 0.5 magnitude bins
+
+#Graph embellishments
+params = {
+   'axes.labelsize': 20,
+   'axes.titlesize': 20,
+   'font.size': 20,
+   'font.family': 'serif',
+   'legend.fontsize': 20,
+   'xtick.labelsize': 20,
+   'ytick.labelsize': 20,
+   'figure.figsize': [10, 7]
+   }
+plt.rcParams.update(params)
+fam = {'fontname':'Times New Roman'}
+
+#Errors are of the form
+errors = (10**(np.array(bins))*np.log(10)*(1/(2*np.array(bins))))
+
+#Plot the graph using log scales
+x = np.arange(7.5,22,0.5)
+plt.plot(x,10**(np.array(bins)),'kx',label='Experimental values')
+plt.errorbar(x,10**(np.array(bins)),yerr=errors,fmt='kx',ms=10,capsize=5)
+plt.yscale('log') #Place a log scale on the y-axis
+plt.xlabel('Magnitude m')
+plt.ylabel('Cumulative number of galaxies N(m)')
+plt.ylim(0)
+plt.grid()
+plt.xlim(8,22)
+plt.ylim(1,10**5)
+
+
